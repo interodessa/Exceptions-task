f039ac7ea269571e81605748381332939971e763
interodessa <alexkotuz@gmail.com>
2024-02-13 22:52:14 +0200

    rename class App, Added output of random values

--- src/app/App.java
+++ src/app/App.java
@@ -3,7 +3,7 @@ package app;
 import app.devices.DataHandler;
 import app.view.HeightProvider;
 
-public class app {
+public class App {
     public static void main(String[] args) {
         HeightProvider heightProvider = new HeightProvider();
         int height = heightProvider.getHeight();
--- src/app/devices/DataHandler.java
+++ src/app/devices/DataHandler.java
@@ -1,10 +1,14 @@
 package app.devices;
 
 import app.exception.HeightException;
+import java.util.Random;
 
 public class DataHandler {
     private static final int HEIGHT_MIN = 120;
     private static final int HEIGHT_MAX = 355;
+    int Temperature;
+    int Pressure;
+    int Humidity;
     public String airData(int height){
         try {
             if(height < HEIGHT_MIN || height > HEIGHT_MAX){
@@ -13,8 +17,12 @@ public class DataHandler {
         } catch (HeightException e){
             return e.getMessage();
         }
-        return "Temperature:\n" +
-                "Pressure:\n" +
-                "Humidity:\n";
+        Random rand = new Random();
+        Temperature = rand.nextInt(-30,30);
+        Pressure = rand.nextInt(950,1050);
+        Humidity = rand.nextInt(20,100);
+        return "Temperature:" + Temperature + "C\n" +
+                "Pressure:" + Pressure + "гПа\n" +
+                "Humidity:" + Humidity + "%\n";
     }
 }
